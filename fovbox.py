0a2a8bc9128021b31d7a3a91c32d7d0fccdeda4f
Jonathan Sawyer <jon@gina.alaska.edu>
2009-12-11 15:43:43 -0900

    Initial commit for fovbox.py

---
+++ fovbox.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import math
+
+def _usage():
+    """Usage"""
+
+def _main():
+    """Main"""
+    
+    print "Hello world!"
+    
+    return 0
+
+if __name__ == "__main__":
+    _main()
